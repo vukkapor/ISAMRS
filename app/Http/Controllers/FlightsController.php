c6e96d84fcfc7faf53879fb9f25fc40598c26dd9
Dejan <dejangacevic@gmail.com>
2019-04-01 00:38:57 +0200

    Bug fix TASK2.4.2

--- app/Http/Controllers/FlightsController.php
+++ app/Http/Controllers/FlightsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 use \App\Flight;
 
 class FlightsController extends Controller
@@ -22,8 +23,9 @@ class FlightsController extends Controller
 
 	public function store()
 	{
+		$current_date = Carbon::now()->format('Y-m-d');
 		$attributes = request()->validate([
-			'flight_departure_date' => 'required|date',
+			'flight_departure_date' => 'required|date|after_or_equal:' . $current_date,
 			'flight_departure_time' => 'required',
 			'flight_arrival_date' => 'required|date|after_or_equal:flight_departure_date',
 			'flight_arrival_time' => 'required',
