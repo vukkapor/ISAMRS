11c47f1cde734921a2c0ce0806598d4cb53ba357
Dejan <dejangacevic@gmail.com>
2019-03-31 19:23:20 +0200

    Added backend validation rules

--- app/Http/Controllers/FlightsController.php
+++ app/Http/Controllers/FlightsController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\Flight;
 
 class FlightsController extends Controller
 {
-    //
+    public function index()
+    {
+    	//$flights = Flight::all();
+
+    	return view('flights.index');
+    }
+
+    public function create()
+    {
+    	return view('flights.create');
+    }
+
+	public function store()
+	{
+		$attributes = request()->validate([
+			'flight_departure_date' => 'required|date',
+			'flight_departure_time' => 'required',
+			'flight_arrival_date' => 'required|date|after_or_equal:flight_departure_date',
+			'flight_arrival_time' => 'required',
+			'flight_transfers' => 'required|alpha_num',
+			'flight_transfers_location' => 'required|filled|max:255',
+			'flight_price' => 'required|min:1',
+		]);
+
+		// Flight::create($attributes);
+
+		return redirect('/projects');
+	}    
 }
